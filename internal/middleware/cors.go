46edd3076790f08723b546596e4d958c4e113818
roslava <48380940+roslava@users.noreply.github.com>
2026-07-02 23:51:19 +0300

    added http://localhost:5173

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -9,7 +9,7 @@ import (
 
 func CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"}, // или "*"
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"}, // или "*"
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "X-API-Key", "Authorization", "Accept", "Content-Length", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
